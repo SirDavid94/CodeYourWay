803bac25a254db807ae7a3c9edf91254dc16ce16
saqibn <saqibn@vcu.edu>
2019-03-14 17:38:29 -0400

    created random facts page

---
+++ app/src/main/java/vcu/cmsc355/codeyourway/randomFacts.java
@@ -0,0 +1,13 @@
+package vcu.cmsc355.codeyourway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class randomFacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_facts);
+    }
+}
