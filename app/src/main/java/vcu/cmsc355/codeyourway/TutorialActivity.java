34ec3a9f1f9c95e66e777939b099dd96774d4f1c
SirDavid94 <43501979+SirDavid94@users.noreply.github.com>
2019-03-14 19:19:01 -0400

    Tutorial Activity

--- app/src/main/java/vcu/cmsc355/codeyourway/TutorialActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/TutorialActivity.java
@@ -1,5 +1,37 @@
 package vcu.cmsc355.codeyourway;
 
-public class TutorialActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+public class TutorialActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorialpage);
+
+        Button BtNext;
+        Button BtSkip;
+
+        BtNext = (Button) findViewById(R.id.nextButton);
+        BtSkip = (Button) findViewById(R.id.skipButton);
+
+        //BtNext.setOnClickListener(this);
+        //BtSkip.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.nextButton:
+                Toast.makeText(this, "Button Under Development", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nextbttn:
+                Toast.makeText(this, "Button Under Development", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
 }
