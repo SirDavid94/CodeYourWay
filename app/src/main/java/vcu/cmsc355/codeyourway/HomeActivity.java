08845b97035434c488ad5f500820264e1883d32d
SirDavid94 <43501979+SirDavid94@users.noreply.github.com>
2019-03-15 12:01:25 -0400

    Merge branch 'master' of C:\Users\donta\Desktop\project-a-group-3-team-ios with conflicts.

--- app/src/main/java/vcu/cmsc355/codeyourway/HomeActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/HomeActivity.java
@@ -1,13 +1,29 @@
 package vcu.cmsc355.codeyourway;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button elementaryProgram;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
+        elementaryProgram = (Button)findViewById(R.id.elementaryProgramming);
+        elementaryProgram.setOnClickListener(new View.OnClickListener() {                        //take to the settings page
+            public void onClick(View v) {
+                Intent elementaryIntent = new Intent(HomeActivity.this, TutorialActivity.class);
+                startActivity(elementaryIntent);
+            }
+        });
+
     }
+
 }
--- app/src/main/java/vcu/cmsc355/codeyourway/TutorialActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/TutorialActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TutorialActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class TutorialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorialpage);
 
+        TextView tutorialView;
         Button BtNext;
         Button BtSkip;
 
