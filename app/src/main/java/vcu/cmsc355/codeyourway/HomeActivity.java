e9441d7b682e1fce2a7de58333c9f4fa9e68da5a
SirDavid94 <43501979+SirDavid94@users.noreply.github.com>
2019-03-19 11:28:11 -0400

    Module completion page

--- app/src/main/java/vcu/cmsc355/codeyourway/HomeActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/HomeActivity.java
@@ -27,35 +27,35 @@ public class HomeActivity extends AppCompatActivity {
         arrays = (Button)findViewById(R.id.SingleDimArray);
 
 
-        elementaryProgram.setOnClickListener(new View.OnClickListener() {                        //take to the settings page
+        elementaryProgram.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent elementaryIntent = new Intent(HomeActivity.this, IntroTutorialActivity.class);
                 startActivity(elementaryIntent);
             }
         });
 
-        loops.setOnClickListener(new View.OnClickListener() {                        //take to the settings page
+        loops.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent elementaryIntent = new Intent(HomeActivity.this, LoopTutorialActivity.class);
                 startActivity(elementaryIntent);
             }
         });
 
-        selections.setOnClickListener(new View.OnClickListener() {                        //take to the settings page
+        selections.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent elementaryIntent = new Intent(HomeActivity.this, SelectionsTutorialActivity.class);
                 startActivity(elementaryIntent);
             }
         });
 
-        mathFunctions.setOnClickListener(new View.OnClickListener() {                        //take to the settings page
+        mathFunctions.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent elementaryIntent = new Intent(HomeActivity.this, MathFuncTutorialActivity.class);
                 startActivity(elementaryIntent);
             }
         });
 
-        arrays.setOnClickListener(new View.OnClickListener() {                        //take to the settings page
+        arrays.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent elementaryIntent = new Intent(HomeActivity.this, ArraysTutorialActivity.class);
                 startActivity(elementaryIntent);
