a167d7725384e182fbd3eadfe1b03ef5e07e3ebd
Abdulateef Oladele <oladelea@vcu.edu>
2019-03-19 13:25:55 -0400

    added button to homepage

--- app/src/main/java/vcu/cmsc355/codeyourway/HomeActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/HomeActivity.java
@@ -3,8 +3,11 @@ package vcu.cmsc355.codeyourway;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -17,7 +20,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
 
 
         elementaryProgram = (Button)findViewById(R.id.introToProgramming);
@@ -61,6 +63,39 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(elementaryIntent);
             }
         });
+
+    }
+
+
+    @Override
+
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId())  {
+            case R.id.menuLogout:
+                Toast.makeText(this, "Logging user out", Toast.LENGTH_SHORT).show();
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+            case R.id.menuSettings:
+                startActivity(new Intent(this,SettingsActivity.class));
+                break;
+
+            case R.id.menuProfile:
+                Toast.makeText(this, "Opening user profile", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, CreateProfileActivity.class));
+                finish();
+                break;
+        }
+        return true;
 
     }
 
