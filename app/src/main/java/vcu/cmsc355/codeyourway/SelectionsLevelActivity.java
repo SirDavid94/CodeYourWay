08051a5ca544004010ca8c8ff74d2f5371653d67
yime <yime@vcu.edu>
2019-03-20 20:26:45 -0400

    Added java code for "selection level"

--- app/src/main/java/vcu/cmsc355/codeyourway/SelectionsLevelActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/SelectionsLevelActivity.java
@@ -1,13 +1,17 @@
 package vcu.cmsc355.codeyourway;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SelectionsLevelActivity extends AppCompatActivity {
+public class SelectionsLevelActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selections_level);
-    }
-}
+
+
