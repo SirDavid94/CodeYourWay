7017b6317bc63a7792551ebf81f0162d437ae668
saqibn <saqibn@vcu.edu>
2019-03-23 02:12:36 -0400

    did something, dont know what

--- app/src/main/java/vcu/cmsc355/codeyourway/MCquestionActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/MCquestionActivity.java
@@ -4,8 +4,8 @@ Nazmus Saqib
 package vcu.cmsc355.codeyourway;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -33,14 +33,14 @@ public class MCquestionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mcquestion);
 
         db = new DatabaseHelper(this);
-        level_header    = (TextView) findViewById(R.id.levelnumber);               //display current level
-        questions_num   = (TextView) findViewById(R.id.question_header);          //Shows the question number
-        submit          = (Button) findViewById(R.id.submit);                    //submit button
-        question        = (TextView) findViewById(R.id.actual_question);        //shows the question
-        answer1         = (RadioButton) findViewById(R.id.MCQ_radioButton1);   //answers to choose
-        answer2         = (RadioButton) findViewById(R.id.MCQ_radioButton2);
-        answer3         = (RadioButton) findViewById(R.id.MCQ_radioButton3);
-        answer4         = (RadioButton) findViewById(R.id.MCQ_radioButton4);
+        level_header = (TextView) findViewById(R.id.levelnumber);               //display current level
+        questions_num = (TextView) findViewById(R.id.question_header);          //Shows the question number
+        submit = (Button) findViewById(R.id.submit);                    //submit button
+        question = (TextView) findViewById(R.id.actual_question);        //shows the question
+        answer1 = (RadioButton) findViewById(R.id.MCQ_radioButton1);   //answers to choose
+        answer2 = (RadioButton) findViewById(R.id.MCQ_radioButton2);
+        answer3 = (RadioButton) findViewById(R.id.MCQ_radioButton3);
+        answer4 = (RadioButton) findViewById(R.id.MCQ_radioButton4);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,7 +50,14 @@ public class MCquestionActivity extends AppCompatActivity {
 
     }
 
-    private void getAnswers(String question, String first, String second, String third, String fourth) { //this method should all the questions and answers. the answers will be placed randomly
+    /**
+     * @param question
+     * @param first
+     * @param second
+     * @param third
+     * @param fourth
+     */
+    public void getAnswers(String question, String first, String second, String third, String fourth) { //this method should all the questions and answers. the answers will be placed randomly
 
         this.question.setText(question);// show the question in the text box
 
@@ -61,22 +68,22 @@ public class MCquestionActivity extends AppCompatActivity {
         }
         Collections.shuffle(Arrays.asList(random_numbers)); /*putting random numbers in the array
                                                                 to show the answer choice randomly*/
-        if(random_numbers[0]==0){       //placing the answers randomly
+        if (random_numbers[0] == 0) {       //placing the answers randomly
             answer1.setText(first);     //(I am sorry if I did bad job, also tried to make it as simple as possible)
             answer2.setText(second);
             answer3.setText(third);
             answer4.setText(fourth);
-        }else if(random_numbers[0]==1){
+        } else if (random_numbers[0] == 1) {
             answer1.setText(fourth);
             answer2.setText(third);
             answer3.setText(second);
             answer4.setText(first);
-        }else if(random_numbers[0]==3){
+        } else if (random_numbers[0] == 3) {
             answer1.setText(second);
             answer2.setText(first);
             answer3.setText(fourth);
             answer4.setText(third);
-        }else if(random_numbers[0]==4){
+        } else if (random_numbers[0] == 4) {
             answer1.setText(third);
             answer2.setText(fourth);
             answer3.setText(first);
