61a667c3562b4e9f7b19249372443140cd86ebc3
yime <yime@vcu.edu>
2019-03-26 18:58:54 -0400

    Started working on UML for "change password"

---
+++ app/src/main/java/vcu/cmsc355/codeyourway/ChangePasswordActivity.java
@@ -0,0 +1,37 @@
+package vcu.cmsc355.codeyourway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    EditText currentPassword, newPassword, confirmPassword;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+
+        currentPassword = (EditText)findViewById(R.id.current_pass);
+        newPassword = (EditText)findViewById((R.id.new_pass));
+        confirmPassword = (EditText)findViewById((R.id.confirm_pass));
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void change(View v) {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user!=null)
+        {
+            user.delete();
+
+        }
+
+
+    }
+}
