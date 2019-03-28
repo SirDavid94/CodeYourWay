a95c949de93f4ee91a78fd217ecdcfe28e0a2b84
Abdulateef Oladele <oladelea@vcu.edu>
2019-03-27 22:44:17 -0400

    Fixed Profile page error

--- app/src/main/java/vcu/cmsc355/codeyourway/LeaderBoard.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/LeaderBoard.java
@@ -1,7 +1,10 @@
 package vcu.cmsc355.codeyourway;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LeaderBoard extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class LeaderBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+
+
+        Button homeButton = findViewById(R.id.HallOfFameHomeButton);
+
+
+
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LeaderBoard.this, HomeActivity.class));
+            }
+        });
     }
+
 }
