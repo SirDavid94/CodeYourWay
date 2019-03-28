d18459bbaf098fc4b17036ae059343745e4434dd
yime <yime@vcu.edu>
2019-03-28 16:52:11 -0400

    Changing code to Change Password

--- app/src/main/java/vcu/cmsc355/codeyourway/ChangePasswordActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/ChangePasswordActivity.java
@@ -3,6 +3,8 @@ package vcu.cmsc355.codeyourway;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,10 +13,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class ChangePasswordActivity extends AppCompatActivity {
+public class ChangePasswordActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText currentPassword, newPassword, confirmPassword;
     FirebaseAuth auth;
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,27 +27,17 @@ public class ChangePasswordActivity extends AppCompatActivity {
         currentPassword = (EditText)findViewById(R.id.current_pass);
         newPassword = (EditText)findViewById((R.id.new_pass));
         confirmPassword = (EditText)findViewById((R.id.confirm_pass));
-        auth = FirebaseAuth.getInstance();
+
+        button1 = findViewById(R.id.btn_submit);
+
+        button1.setOnClickListener(this);
     }
 
-    public void change(View v) {
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user!=null)
-        {
-           user.updatePassword(confirmPassword.getText().toString())
-                   .addOnCompleteListener(new OnCompleteListener<Void>() {
-                       @Override
-                       public void onComplete(@NonNull Task<Void> task) {
-                           if(task.isSuccessful())
-                           {
-                               Toast.makeText(getApplicationContext())
-                           }
-                       }
-                   })
-
-        }
+     button1.setOnClickListener(new View.OnClickListener() {
 
 
-    }
+}
+
+
 }
