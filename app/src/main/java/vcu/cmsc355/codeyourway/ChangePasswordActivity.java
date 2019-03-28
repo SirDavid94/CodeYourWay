d7674898d8cbb5e87d00c7f867f47a18f9c570a4
yime <yime@vcu.edu>
2019-03-28 17:30:51 -0400

    Working on java code for Change Password

--- app/src/main/java/vcu/cmsc355/codeyourway/ChangePasswordActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/ChangePasswordActivity.java
@@ -1,22 +1,26 @@
 package vcu.cmsc355.codeyourway;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
+import com.rengwuxian.materialedittext.MaterialEditText;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class ChangePasswordActivity extends AppCompatActivity implements View.OnClickListener {
+public class ChangePasswordActivity extends AppCompatActivity {
 
     EditText currentPassword, newPassword, confirmPassword;
-    FirebaseAuth auth;
+    //FirebaseAuth auth;
     Button button1;
 
     @Override
@@ -24,20 +28,37 @@ public class ChangePasswordActivity extends AppCompatActivity implements View.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
 
-        currentPassword = (EditText)findViewById(R.id.current_pass);
-        newPassword = (EditText)findViewById((R.id.new_pass));
-        confirmPassword = (EditText)findViewById((R.id.confirm_pass));
+        currentPassword = (EditText) findViewById(R.id.current_pass);
+        newPassword = (EditText) findViewById((R.id.new_pass));
+        confirmPassword = (EditText) findViewById((R.id.confirm_pass));
 
         button1 = findViewById(R.id.btn_submit);
 
-        button1.setOnClickListener(this);
-    }
 
 
-     button1.setOnClickListener(new View.OnClickListener() {
+ button1.setOnClickListener(new View.OnClickListener() {
+     @Override
+     public void onClick(View v) {
+            updatePassword();
+
+     }
+ });
 
 
 }
+
+
+        public void updatePassword() {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(ChangePasswordActivity.this);
+            alertDialog.setTitle("Change Password");
+            alertDialog.setMessage("Please fill all information");
+
+            LayoutInflater inflater = LayoutInflater.from(this);
+            View layout_pwd = inflater.inflate(R.layout.activity_change_password,null);
+
+            MaterialEditText password = (MaterialEditText) layout_pwd.findViewById(current_pass)
+            MaterialEditText password = (MaterialEditText) layout_pwd.findViewById(new)
+    }
 
 
 }
