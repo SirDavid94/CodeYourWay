f544228af970e2e9b61301b865aa77d6e15318b8
yime <yime@vcu.edu>
2019-04-01 15:02:26 -0400

    Working on java code for Change Password

--- app/src/main/java/vcu/cmsc355/codeyourway/ChangePasswordActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/ChangePasswordActivity.java
@@ -3,62 +3,71 @@ package vcu.cmsc355.codeyourway;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.rengwuxian.materialedittext.MaterialEditText;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class ChangePasswordActivity extends AppCompatActivity {
 
     EditText currentPassword, newPassword, confirmPassword;
-    //FirebaseAuth auth;
-    Button button1;
+    FirebaseUser firebaseUser;
+    Button submit;
+    FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
 
-        currentPassword = (EditText) findViewById(R.id.current_pass);
-        newPassword = (EditText) findViewById((R.id.new_pass));
-        confirmPassword = (EditText) findViewById((R.id.confirm_pass));
+        currentPassword = findViewById(R.id.current_pass);
+        newPassword = findViewById((R.id.new_pass));
+        confirmPassword = findViewById((R.id.confirm_pass));
 
-        button1 = findViewById(R.id.btn_submit);
+        submit = findViewById(R.id.btn_submit);
+
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        String userCurrent_Password = currentPassword.getText().toString();
+        String userNew_Password = newPassword.getText().toString();
+        String userConfirm_Password = confirmPassword.getText().toString();
+        firebaseUser.updatePassword(userCurrent_Password,userNew_Password,userNew_Password).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(ChangePasswordActivity.this, "Password Changed",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+
+                else {
+
+                    Toast.makeText(ChangePasswordActivity.this, "Update failed",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
 
 
 
- button1.setOnClickListener(new View.OnClickListener() {
-     @Override
-     public void onClick(View v) {
-            updatePassword();
 
-     }
- });
+
 
 
 }
 
 
-        public void updatePassword() {
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(ChangePasswordActivity.this);
-            alertDialog.setTitle("Change Password");
-            alertDialog.setMessage("Please fill all information");
 
-            LayoutInflater inflater = LayoutInflater.from(this);
-            View layout_pwd = inflater.inflate(R.layout.activity_change_password,null);
-
-            MaterialEditText password = (MaterialEditText) layout_pwd.findViewById(current_pass)
-            MaterialEditText password = (MaterialEditText) layout_pwd.findViewById(new)
-    }
 
 
 }
