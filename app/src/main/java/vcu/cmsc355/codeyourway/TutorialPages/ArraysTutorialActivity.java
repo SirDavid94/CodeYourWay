0891a7bdf7f34bf8cb3e69af269b437b2a4eca14
SirDavid94 <43501979+SirDavid94@users.noreply.github.com>
2019-04-08 12:47:56 -0400

    Minor edits

--- app/src/main/java/vcu/cmsc355/codeyourway/TutorialPages/ArraysTutorialActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/TutorialPages/ArraysTutorialActivity.java
@@ -4,12 +4,20 @@ package vcu.cmsc355.codeyourway.TutorialPages;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import vcu.cmsc355.codeyourway.GameLevel.LevelSelectionActivity;
+import vcu.cmsc355.codeyourway.HomeActivity;
+import vcu.cmsc355.codeyourway.LeaderBoardActivity;
+import vcu.cmsc355.codeyourway.LoginActivity;
+import vcu.cmsc355.codeyourway.ProfileActivity;
 import vcu.cmsc355.codeyourway.R;
+import vcu.cmsc355.codeyourway.SettingsActivity;
 
 public class ArraysTutorialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,28 +25,57 @@ public class ArraysTutorialActivity extends AppCompatActivity {
         setContentView(R.layout.activity_arrays);
 
 
-        TextView tutorialView;
-        TextView moduleID;
         Button BtContinue;
-
-
         BtContinue = (Button) findViewById(R.id.ContinueArrays);
-        moduleID = findViewById(R.id.ArraysModuleID);
 
         BtContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent levelSelection = new Intent(ArraysTutorialActivity.this, LevelSelectionActivity.class);
-                levelSelection.putExtra("moduleID",5);
+                levelSelection.putExtra("moduleID", 5);
                 startActivity(levelSelection);
             }
         });
 
     }
 
+    @Override
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
 
+        return true;
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuLogout:
+                Toast.makeText(this, "Logging user out", Toast.LENGTH_SHORT).show();
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+            case R.id.menuSettings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+
+            case R.id.menuProfile:
+                Toast.makeText(this, "Opening user profile", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ArraysTutorialActivity.this, ProfileActivity.class));
+                finish();
+                break;
+
+            case R.id.HallOfFame:
+                Toast.makeText(this, "Opening LeaderBoardActivity", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, LeaderBoardActivity.class));
+                finish();
+                break;
+        }
+        return true;
+
+    }
 }
 
 
