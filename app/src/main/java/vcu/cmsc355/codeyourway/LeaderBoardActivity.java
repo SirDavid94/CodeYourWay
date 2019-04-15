b47caed88af5a163e4985c5c2b17f2bfc981cacf
Abdulateef Oladele <oladelea@vcu.edu>
2019-04-15 18:03:26 -0400

    Comopleted Leaderboard Activity and Hall of Fame Page

--- app/src/main/java/vcu/cmsc355/codeyourway/LeaderBoardActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/LeaderBoardActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,9 +22,13 @@ import vcu.cmsc355.codeyourway.Model.LeaderBoard;
 public class LeaderBoardActivity extends AppCompatActivity {
 
     String username;
-    int score;
+    int score =0;
+    int i =9;
     FirebaseDatabase database;
     DatabaseReference AwardsDatabase;
+    TextView rank1,rank2,rank3,rank4,rank5,rank6,rank7,rank8,rank9,rank10;
+    TextView score1,score2,score3,score4,score5,score6,score7,score8,score9,score10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,8 @@ public class LeaderBoardActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         AwardsDatabase = database.getReference("Awards");
         Button homeButton = findViewById(R.id.HallOfFameHomeButton);
+
+
 
        UpdateScore();
 
@@ -56,13 +63,17 @@ public class LeaderBoardActivity extends AppCompatActivity {
                          for ( DataSnapshot data: dataSnapshot.getChildren())
                          {
                              Awards rank = data.getValue(Awards.class);
-                             username = rank.getUser();
-                             score = rank.getAwardCount();
+                             username = data.getKey();
+                             score = (int)(rank.getAwardCount());
+
+                             ShowLeaderBoard(i);
+                             i--;
 
                          }
 
                         LeaderBoard list = new LeaderBoard(username,score);
-                        ShowLeaderBoard();
+
+
                     }
 
                     @Override
@@ -73,7 +84,36 @@ public class LeaderBoardActivity extends AppCompatActivity {
 
     }
 
-    private void ShowLeaderBoard() {
+    private void ShowLeaderBoard(int num) {
+
+        TextView [] rankList ={
+                rank1 = findViewById(R.id.rank1),
+                rank2 = findViewById(R.id.rank2),
+                rank3 = findViewById(R.id.rank3),
+                rank4 = findViewById(R.id.rank4),
+                rank5 = findViewById(R.id.rank5),
+                rank6 = findViewById(R.id.rank6),
+                rank7 = findViewById(R.id.rank7),
+                rank8 = findViewById(R.id.rank8),
+                rank9 = findViewById(R.id.rank9),
+                rank10 = findViewById(R.id.rank10)
+        };
+
+        TextView [] scoreList ={
+                score1 = findViewById(R.id.score1),
+                score2 = findViewById(R.id.score2),
+                score3 = findViewById(R.id.score3),
+                score4 = findViewById(R.id.score4),
+                score5 = findViewById(R.id.score5),
+                score6 = findViewById(R.id.score6),
+                score7 = findViewById(R.id.score7),
+                score8 = findViewById(R.id.score8),
+                score9 = findViewById(R.id.score9),
+                score10 = findViewById(R.id.score10)
+        };
+
+        rankList[num].setText(username);
+        scoreList[num].setText(" "+score+" ");
 
 
     }
