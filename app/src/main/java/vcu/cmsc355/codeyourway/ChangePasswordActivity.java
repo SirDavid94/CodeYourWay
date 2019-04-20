a5c8f916782f59abe99a47b7f321c536435e616c
Abdulateef Oladele <oladelea@vcu.edu>
2019-04-19 22:11:32 -0400

    Edited Change password page

--- app/src/main/java/vcu/cmsc355/codeyourway/ChangePasswordActivity.java
+++ app/src/main/java/vcu/cmsc355/codeyourway/ChangePasswordActivity.java
@@ -6,32 +6,32 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import vcu.cmsc355.codeyourway.Model.Common;
+import vcu.cmsc355.codeyourway.Model.User;
 import vcu.cmsc355.codeyourway.TutorialPages.ArraysTutorialActivity;
+import vcu.cmsc355.codeyourway.WalkThrough.WalkThroughActivity;
 
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
-    EditText currentPassword, newPassword, confirmPassword;
+    public EditText currentPassword, newPassword, confirmPassword;
     TextView instructionAlert;
-    FirebaseUser firebaseUser;
+    final String pwd = "password";
     Button submit;
-    FirebaseDatabase firebaseDatabase;
+    FirebaseDatabase Database;
+    DatabaseReference PasswordChangeReference;
     String userCurrentPassword, userNewPassword, userConfirmPassword;
 
     @Override
@@ -45,44 +45,58 @@ public class ChangePasswordActivity extends AppCompatActivity {
         instructionAlert = findViewById(R.id.PasswordInstruction);
         submit = findViewById(R.id.btn_submit);
 
+        Database = FirebaseDatabase.getInstance();
+        PasswordChangeReference = Database.getReference("Users");
 
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        firebaseDatabase = FirebaseDatabase.getInstance();
-
-        userCurrentPassword = currentPassword.getText().toString();
-         userNewPassword = newPassword.getText().toString();
-        userConfirmPassword = confirmPassword.getText().toString();
-
-
+        userCurrentPassword = currentPassword.getText().toString().trim();
+         userNewPassword = newPassword.getText().toString().trim();
+        userConfirmPassword = confirmPassword.getText().toString().trim();
 
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if ( PasswordLengthCheck() && PasswordMatchCheck())
-                {
-                    //Password Replace code goes here
-                    firebaseUser.updatePassword(userConfirmPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()){
-                                Toast.makeText(ChangePasswordActivity.this, "Password Changed",Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
+                if ( !(PasswordLengthCheck() || PasswordMatchCheck())) {
 
-                            else {
-
-                                Toast.makeText(ChangePasswordActivity.this, "Update failed",Toast.LENGTH_SHORT).show();
-
-                            }
-                        }
-                    });
+                    CompletePasswordChange(currentPassword.getText().toString().trim());
 
                 }
+
             }
         });
         }
+
+    private void CompletePasswordChange(String password) {
+
+        PasswordChangeReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User AccountData = dataSnapshot.child(Common.getCurrentUser()).getValue(User.class);
+
+                if (AccountData.getPassword().equals(userCurrentPassword)) {
+
+                    //use set value function to update database
+                    PasswordChangeReference.child(Common.getCurrentUser()).child(pwd).setValue(userConfirmPassword);
+
+                    Toast.makeText(ChangePasswordActivity.this, "Password Changed", Toast.LENGTH_SHORT).show();
+                    finish();
+                    startActivity(new Intent(ChangePasswordActivity.this, LoginActivity.class));
+
+                } else {
+                    Toast.makeText(ChangePasswordActivity.this, "Incorrect Password!!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
 
     /**
      * Checks if the user input and the confirmation matches
@@ -98,27 +112,20 @@ public class ChangePasswordActivity extends AppCompatActivity {
         return true;
     }
 
+
+
     /**
      * Checks the new password string to ensure it is longer than 6 characters
      * @return boolean true if password is longer than 6 characters and false if otherwise
      */
     private boolean PasswordLengthCheck() {
 
-        if (userCurrentPassword.length() < 6) {
+        if (userNewPassword.length() < 6) {
             instructionAlert.setTextColor(Color.RED);
             Toast.makeText(this, "Password must be longer than 6 characters", Toast.LENGTH_SHORT).show();
         }
         return true;
     }
-    @Override
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-
-        return true;
-
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
